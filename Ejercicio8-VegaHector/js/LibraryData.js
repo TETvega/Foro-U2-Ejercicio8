f9bbaccc938297339813fa43c0c0f6bebb523cb7
Hector Vega <hectormartinez1vg@gmail.com>
2024-03-29 11:55:09 -0600

    fix table, add list categories, fix shear table

--- Ejercicio8-VegaHector/js/LibraryData.js
+++ Ejercicio8-VegaHector/js/LibraryData.js
@@ -19,44 +19,93 @@ function retornaUsuarioActual() {
 }
 
 
-// Para la carga de listas 
 
-// ------------------------------------------------ 
+// Para la carga de listas  e INICIALIZACION DE LAS TABLAS 
+// Variables para saber como se enuentra la tabla 
+let dataTable;
+let dataTableIsInitialized = false;
 
-const listTask = async() => {
+// Opnciones escitas al la tabla 
+const dataTableOptions = {
+    //scrollX: "2000px",
+    lengthMenu: [5, 10, 15, 20], // cantidad de resgistros pr pagina
+    columnDefs: [
+        { className: "centered", targets: [0, 1, 2, 3] },
+        { orderable: false, targets: [0,2] },
+        { searchable: false, targets: [2,3] }
+        //{ width: "50%", targets: [0] }
+    ],
+    // pageLength: 3,
+    destroy: true,
+    language: {
+        lengthMenu: "Mostrar _MENU_ registros por página",
+        zeroRecords: "Ningún usuario encontrado",
+        info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
+        infoEmpty: "Ningún usuario encontrado",
+        infoFiltered: "(filtrados desde _MAX_ registros totales)",
+        search: "Buscar:",
+        loadingRecords: "Cargando...",
+        paginate: {
+            first: "Primero",
+            last: "Último",
+            next: "Siguiente",
+            previous: "Anterior"
+        }
+    }
+};
 
-    try {
-        // variable que tiene toda la informacion del usuarios actual
-        const infoUser = retornaUsuarioActual();
-        const lista = await infoUser.taskList
-        console.log(lista);
-
-        let contenido =  ''
-
-        // itera sobre la cantidad de objetos que existen 
-        lista.forEach( (producto, index) => {
-            // contendio de la tabla
-            contenido += 
-            `<tr>
-                <td> ${index + 1}</td>
-                <td> ${producto.nombre}</td>
-                <td> ${producto.categoria}</td>
-                <td> ${producto.cantidad}</td>
-                <td> ${producto.estado}</td>
-            </tr>`;
-        });
-        // Insertar el contenido en la tabla
-        document.getElementById("DataTableBody_ListasProductos").innerHTML = contenido;
-
-    } catch (error) {
-        alert(error)
+const initDataTable = async () => {
+    if (dataTableIsInitialized) {
+        dataTable.destroy();
     }
 
-}
-// para cargar el contenido preexistente en la data del usuario 
-window.addEventListener("load" , async() => {
-    await listTask()
-})
+    await listProducts();
+
+    dataTable = $("#dataTable_ListasProductos").DataTable(dataTableOptions);
+
+    dataTableIsInitialized = true;
+};
+
+const listProducts = async () => {
+    try {
+        const response = await retornaUsuarioActual();
+        const productos = await response.taskList;
+        const categorias = await response.categorias
+        console.log(productos);
+        console.log(categorias);
+
+        let selecCategorias = `<option value="">Seleccionar...</option>`;
+        let content = ``;
+        productos.forEach((producto, index) => {
+            content += `
+                <tr>
+                    <td>${producto.nombre}</td>
+                    <td>${producto.categoria}</td>
+                    <td>${producto.cantidad}</td>
+                    <td>${producto.estado}</td>
+                    <td class="center">
+                        <button class="btn btn-sm btn-primary btn-edit"><i class="fa-solid fa-pencil"></i>Edit</button>
+                    </td>
+                </tr>`;
+        });
+
+        categorias.forEach( (categoria) => {
+
+            selecCategorias +=`
+            <option value="${categoria}">${categoria}</option>
+            `
+            
+        })
+        tableBody_ListasProductos.innerHTML = content;
+        selectCategoria.innerHTML = selecCategorias;
+    } catch (ex) {
+        alert(ex);
+    }
+};
+
+window.addEventListener("load", async () => {
+    await initDataTable();
+});
 
 
 
@@ -76,11 +125,16 @@ document.getElementById('selectCategoria').addEventListener('change', function()
 
 // Boton que crea una nueva producto
 const btnCreateProduct = document.getElementById("BTN_crearProducto")
-
+const btnCleanForm = document.getElementById("BTN_limpiarFormulario")
 
 
 
 // APARTADO DE EVENTOS POR BOTONES ------------------------------------------------------------
+let btnCrearIsDelete=false;
+btnCleanForm.addEventListener("click" , () => {
+    // Limpiar el formulario
+    document.getElementById("form_Product").reset();
+})
 
 btnCreateProduct.addEventListener("click" , async() => {
 //form_Product
@@ -150,7 +204,7 @@ try {
                 // mensaje de correcto
                 alert("Se ha agregado un nuevo producto correctamente");
 
-                await listTask(); 
+                await initDataTable(); 
             
 } catch (error) {
     alert(error)
@@ -158,7 +212,53 @@ try {
 
 })
 
+// PARA LOS BOTONES DE ESITAR EN LA TABLA
+// esta funcion esta pendiente de todo el documento y espera un evento 
+// verifica si el evento es un click en una targeta que tenga la clase de bnt-edit 
+document.addEventListener("click", function(e) {
+    if (e.target && e.target.classList.contains("btn-edit")) {
+        // Obtener los datos segun la row en la que se preciono el click
+        const row = e.target.closest("tr");
+        // asignando valores a las constantes 
+        const nombre = row.cells[0].textContent;
+        const categoria = row.cells[1].textContent;
+        const cantidad = row.cells[2].textContent;
+        const estado = row.cells[3].textContent;
 
+        // llenando el formulario con los datos de la fila de la tabla
+        document.getElementById("nombre_Producto").value = nombre;
+        document.getElementById("categoria_Producto").value = categoria;
+        document.getElementById("cantidad_Producto").value = cantidad;
+        document.getElementById("estado_Producto").value = estado;
+
+        // Ocultando los botones de crear y mostrar los de editar y eliminar 
+        document.getElementById("BTN_crearProducto").remove();
+        btnCrearIsDelete=true
+        document.getElementById("BTN_limpiarFormulario").addEventListener("click" , () => {
+            // Limpiar el formulario
+            document.getElementById("form_Product").reset();
+            // Función para crear el btn crear 
+            
+            if (btnCrearIsDelete) {
+                const newButton = document.createElement("button"); 
+                newButton.setAttribute("type", "button"); 
+                newButton.setAttribute("id", "BTN_crearProducto"); 
+                newButton.classList.add("btn", "btn-primary", "d-inline-block", "mx-5", "mb-4"); 
+                newButton.innerHTML = '<i class="bi bi-plus-circle"></i> Crear'; 
+                const divContenedor = document.querySelector('.col-md.text-center');
+                const segundoElemento = divContenedor.children[1];
+                divContenedor.insertBefore(newButton, segundoElemento);
+                document.getElementById("BTN_modificarProducto").style.display = "none";
+                document.getElementById("BTN_eliminarProducto").style.display = "none";
+                btnCrearIsDelete=false
+            }
+               
+            
+        })
+        document.getElementById("BTN_modificarProducto").style.display = "inline-block";
+        document.getElementById("BTN_eliminarProducto").style.display = "inline-block";
+    }
+});
 
 // FUNCIONES ----------------- 
 
