5d896e4ac615ec29e9a241f104235eda67caac5f
Hector Vega <hectormartinez1vg@gmail.com>
2024-03-30 00:21:40 -0600

    botones modificar, editar y eliminar funcionando

--- Ejercicio8-VegaHector/js/LibraryData.js
+++ Ejercicio8-VegaHector/js/LibraryData.js
@@ -18,7 +18,7 @@ function retornaUsuarioActual() {
     return usuarioEncontrado;
 }
 
-
+// -----------------------------------------------------------------------------------------
 
 // Para la carga de listas  e INICIALIZACION DE LAS TABLAS 
 // Variables para saber como se enuentra la tabla 
@@ -108,9 +108,9 @@ window.addEventListener("load", async () => {
 });
 
 
-
+// ---------------------------------------------------------------------------------------------------
 // CAMBIAR EN EL FORMULARIO SEGUN SELECIONE ALGO
-
+// lista las categorias existentes 
 document.getElementById('selectCategoria').addEventListener('change', function() {
     // Obtiene el valor seleccionado
     var selectedValue = this.value;
@@ -120,21 +120,34 @@ document.getElementById('selectCategoria').addEventListener('change', function()
 });
 
 
-
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // -----------CONSTANTES BOTONES DEL APARTADO DE Formulario de DATa-----------
+// para saber si existe o no el boton 
+let btnCrearIsDelete=false; 
+let indiceFilaaEditar = -1
 
 // Boton que crea una nueva producto
 const btnCreateProduct = document.getElementById("BTN_crearProducto")
 const btnCleanForm = document.getElementById("BTN_limpiarFormulario")
+const btnModificarProduct = document.getElementById("BTN_modificarProducto")
+const btnElimarProducto = document.getElementById("BTN_eliminarProducto")
 
 
 
 // APARTADO DE EVENTOS POR BOTONES ------------------------------------------------------------
-let btnCrearIsDelete=false;
+
+
+
+// Boton de Limpiar Formulario 
+
 btnCleanForm.addEventListener("click" , () => {
     // Limpiar el formulario
     document.getElementById("form_Product").reset();
 })
+
+
+// boton para crear un producto 
+
 
 btnCreateProduct.addEventListener("click" , async() => {
 //form_Product
@@ -211,6 +224,8 @@ try {
 }
 
 })
+// --------------------- BOTON DE EDITAR EN LAS FILAS DE LA TABLA -------------------------------
+
 
 // PARA LOS BOTONES DE ESITAR EN LA TABLA
 // esta funcion esta pendiente de todo el documento y espera un evento 
@@ -224,7 +239,7 @@ document.addEventListener("click", function(e) {
         const categoria = row.cells[1].textContent;
         const cantidad = row.cells[2].textContent;
         const estado = row.cells[3].textContent;
-
+        indiceFilaaEditar = indiceProductoAEditar(row)
         // llenando el formulario con los datos de la fila de la tabla
         document.getElementById("nombre_Producto").value = nombre;
         document.getElementById("categoria_Producto").value = categoria;
@@ -237,6 +252,7 @@ document.addEventListener("click", function(e) {
         document.getElementById("BTN_limpiarFormulario").addEventListener("click" , () => {
             // Limpiar el formulario
             document.getElementById("form_Product").reset();
+            indiceFilaaEditar =-1
             // Función para crear el btn crear 
             
             if (btnCrearIsDelete) {
@@ -260,6 +276,131 @@ document.addEventListener("click", function(e) {
     }
 });
 
+
+
+// --------------BOTON DE MODIFICAR REGISTRO ------------------
+btnModificarProduct.addEventListener("click", async () => {
+    try {
+        // Obtener los datos del formulario
+        const nombreProducto = document.getElementById("nombre_Producto").value;
+        const categoriaProducto = document.getElementById("categoria_Producto").value;
+        const cantidadProducto = document.getElementById("cantidad_Producto").value;
+        const estadoProducto = document.getElementById("estado_Producto").value;
+
+        // Validar que no se encuentren datos vacios
+        if (!validarCampos(nombreProducto) || !validarCampos(categoriaProducto) || !validarCampos(cantidadProducto) || !validarCampos(estadoProducto)) {
+            alert("Alguno de los campos se encuentra vacío");
+            return;
+        }
+
+
+
+        // obtener el indice del producto que se quiere modificar 
+        const indiceProducto = indiceFilaaEditar;
+        if (indiceProducto === -1) {
+            alert("No se seleciono un producto o el producto es inexistente");
+            return;
+        }
+
+       // obtencion de los datos 
+        const infoUser = retornaUsuarioActual();
+        const listaProductos = infoUser.taskList;
+
+        // actualizando los datos segun el indice que es el producto
+        listaProductos[indiceProducto].nombre = nombreProducto;
+        listaProductos[indiceProducto].categoria = categoriaProducto;
+        listaProductos[indiceProducto].cantidad = cantidadProducto;
+        listaProductos[indiceProducto].estado = estadoProducto;
+
+
+        if (!infoUser.categorias.includes(categoriaProducto)) {
+            // La categoria no existe
+                // CRAER UNA NUEVA CATEGORIA 
+    
+                infoUser.categorias.push(categoriaProducto)
+                alert(`Se creao una nueva categoria: ${categoriaProducto}`);
+
+            }
+
+        // actualizando la losta completa con los cambios del usuario 
+        infoUser.taskList = listaProductos;
+
+        //actualizando todo del local store
+        const usuarios = JSON.parse(localStorage.getItem("usuarios"));
+        // encontrando el indice del usuario 
+        const indiceUsuario = usuarios.findIndex(user => user.fullName === infoUser.fullName);
+
+        // actualizando las listas de productos y demas del usuario
+        usuarios[indiceUsuario] = infoUser;
+
+        localStorage.setItem("usuarios", JSON.stringify(usuarios));
+        eliminarCategoriasSinProductosExistentes()
+
+        // Limpiar el formulario
+        document.getElementById("form_Product").reset();
+
+        // se completo la accion
+        alert("El Producto se modifico correctamente");
+
+        // Actualizar la tabla
+        await initDataTable();
+    } catch (error) {
+        alert(error);
+    }
+});
+
+
+
+btnElimarProducto.addEventListener("click" , async() =>{
+
+try {
+
+         // obtener el indice del producto que se quiere modificar 
+         const indiceProducto = indiceFilaaEditar;
+         if (indiceProducto === -1) {
+             alert("No se seleciono un producto o el producto es inexistente");
+             return;
+         }
+ 
+        // obtencion de los datos 
+         const infoUser = retornaUsuarioActual();
+         const listaProductos = infoUser.taskList;
+
+         // eliminar el producto 
+        const nuevosProductos = [];
+        for (let i = 0; i < listaProductos.length; i++) {
+            if (i !== indiceProducto) {
+                nuevosProductos.push(listaProductos[i]);
+            }
+        }
+
+        // Actualizar la lista de productos en el usuario
+        infoUser.taskList = nuevosProductos;
+
+         //actualizando todo del local store
+         const usuarios = JSON.parse(localStorage.getItem("usuarios"));
+         // encontrando el indice del usuario 
+         const indiceUsuario = usuarios.findIndex(user => user.fullName === infoUser.fullName);
+ 
+         // actualizando las listas de productos y demas del usuario
+         usuarios[indiceUsuario] = infoUser;
+ 
+         localStorage.setItem("usuarios", JSON.stringify(usuarios));
+         eliminarCategoriasSinProductosExistentes()
+         // Limpiar el formulario
+         document.getElementById("form_Product").reset();
+ 
+         // se completo la accion
+         alert("El Producto se Elimino correctamente");
+ 
+         // Actualizar la tabla
+         await initDataTable();
+} catch (error) {
+    alert(error)
+}
+
+} )
+
 // FUNCIONES ----------------- 
 
 
@@ -271,3 +412,48 @@ function validarCampos(campo){
     }
     return true
     }
+
+function indiceProductoAEditar(fila) {
+    const index = fila.rowIndex - 1; 
+    return index;
+}
+
+// funcion que elimina las categorias vacias
+// extrae las categorias del usuario 
+// segun las categorias hace un for que recotrre todos los prodyuctros y si algun producto tiene la actegorias
+// lo almacena en un array temporal que luego de terminar de iterar todas las categorias 
+// actualiza las categorias del usuario 
+function eliminarCategoriasSinProductosExistentes() {
+    // Obtener al usuario 
+    const infoUser = retornaUsuarioActual();
+
+    // LISTAS DE CATEGORIAS Y PRODUCTOS EXISTENTES
+    const categoriasDelUsuario = infoUser.categorias;
+    const listaProductos = infoUser.taskList;
+    // console.log(listProducts);
+    // nueva lista
+    const nuevalistaCategorias = []
+
+    // para todas las categorias has 
+    for (const categoria of categoriasDelUsuario) {
+
+
+        // para ver si tiene almenos un producto
+        const categoriaTieneProducto = listaProductos.some(producto => producto.categoria === categoria)
+        if (categoriaTieneProducto) {
+            nuevalistaCategorias.push(categoria)
+            // console.log(categoria , nuevalistaCategorias);
+        }
+    }
+
+    // actualizando la loista de categorias 
+    infoUser.categorias = nuevalistaCategorias;
+
+    // actualizando el local store
+    const usuarios = JSON.parse(localStorage.getItem("usuarios"))
+
+    const indiceUsuario = usuarios.findIndex(user => user.fullName === infoUser.fullName)
+    // console.log(indiceUsuario);
+    usuarios[indiceUsuario] = infoUser;
+    localStorage.setItem("usuarios", JSON.stringify(usuarios));
+}
